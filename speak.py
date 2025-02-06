e2104fd889676a91c364703a466bd91e373375ca
Felix Cho <felix.cho.kr@gmail.com>
2025-02-06 19:01:44 +0900

    Added external call

--- speak.py
+++ speak.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 from melo.api import TTS
 from pydub import AudioSegment
 from pydub.playback import play
@@ -7,7 +8,9 @@ app = Flask(__name__)
 
 # Initialize the TTS model
 speed = 1.3
-device = 'cpu' # or cuda:0
+device = 'cuda' # or cuda:0
+output_path = '\mnt\d\kr.wav'
+
 model = TTS(language='KR', device=device)
 speaker_ids = model.hps.data.spk2id
 
@@ -19,12 +22,12 @@ def generate_audio():
     if not text:
         return jsonify({'error': 'No text provided'}), 400
 
-    output_path = 'kr.wav'
     model.tts_to_file(text, speaker_ids['KR'], output_path, speed=speed)
 
-    # Play the audio
-    audio = AudioSegment.from_wav(output_path)
-    play(audio)
+    # Send HTTP POST request to http://localhost:8080/
+    response = requests.post('http://localhost:8080/', json={"command": "play", "data": "d:\\kr.wav"})
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to send play command'}), 500
 
     return jsonify({'message': 'Audio played successfully'}), 200
 
